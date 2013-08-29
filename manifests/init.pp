076ef6c6c973431798cbec6cc268a43dbea95b4f
Izzet Mustafaiev <webdizz@gmail.com>
2013-08-29 18:18:15 +0300

    Update init.pp
    Switch to 2.5 version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class aquamacs {
   package { 'Aquamacs':
     provider => 'appdmg',
-    source   => 'http://freefr.dl.sourceforge.net/project/aquamacs/Releases/Aquamacs-Emacs-2.4-UB.dmg',
+    source   => 'http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-2.5.dmg',
   }
 }
